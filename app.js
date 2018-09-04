f63bdd28ced34d6643a0217c03f7baba2b468784
Nicholas Lee <nicholaslee@Nicholass-MacBook-Air-2.local>
2018-09-04 11:54:36 -0600

    added <small> tags at the bottom of job cards

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-
-
 axios.get('https://dino-1-server.herokuapp.com/').then(function(result)   {
     let apiJobData = result.data;
     populateJobListings(apiJobData);
-    let submitButton = document.querySelector('#submit');
-    submitButton.addEventListener('click', function(e)   {
-        let title = document.querySelector('#titleInput').value
-        let pay = document.querySelector('#pay').value
-        let description = document.querySelector('#description').value
-        addToJobListings(title, pay, description);
-        e.preventDefault();
-    })
+})
+
+let submitButton = document.querySelector('#submit');
+submitButton.addEventListener('click', function(e)   {
+    let title = document.querySelector('#titleInput').value
+    let pay = document.querySelector('#pay').value
+    let description = document.querySelector('#description').value
+    addToJobListings(title, pay, description);
+    e.preventDefault();
 })
 
 function populateJobListings(jobData) {
-        for (let i=0; i<jobData.length; i++) {
-            let jobList = document.querySelector('#job-listings');
-            let job = document.createElement('li');
-            job.innerText = jobData[i]['title'];
-            let pay = document.createElement('p');
-            pay.innerHTML = '<small>' + jobData[i]['pay'] + '</small>'
-            job.appendChild(pay);
-            let desc = document.createElement('p');
-            desc.innerText = jobData[i]['description'];
-            job.appendChild(desc);
-            jobList.appendChild(job)
-        }
+    for (let i=0; i<jobData.length; i++) {
+        let jobList = document.querySelector('#job-listings');
+        let job = document.createElement('li');
+        job.innerText = jobData[i]['title'];
+        let pay = document.createElement('p');
+        pay.innerHTML = '<small>' + jobData[i]['pay'] + '</small>'
+        job.appendChild(pay);
+        let desc = document.createElement('p');
+        desc.innerText = jobData[i]['description'];
+        job.appendChild(desc);
+        let interestedDinosaurs = document.createElement('p');
+        interestedDinosaurs.innerHTML = '<small></small>'
+        job.appendChild(interestedDinosaurs);
+        jobList.appendChild(job)
     }
+}
 
 function addToJobListings(title, pay, description) {
     let jobList = document.querySelector('#job-listings');
@@ -38,5 +40,8 @@ function addToJobListings(title, pay, description) {
     let desc = document.createElement('p');
     desc.innerText = description;
     job.appendChild(desc);
+    let interestedDinosaurs = document.createElement('p');
+    interestedDinosaurs.innerHTML = '<small></small>'
+    job.appendChild(interestedDinosaurs);
     jobList.appendChild(job)
 }
