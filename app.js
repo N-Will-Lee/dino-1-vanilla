00b1e9e9c676249c4a4006c62e926cea2feb542c
Nicholas Lee <nicholaslee@Nicholass-MacBook-Air-2.local>
2018-09-04 13:32:07 -0600

    refactored js for readablilty

--- app.js
+++ app.js
@@ -1,40 +1,34 @@
+
+//API request and passing result to job card creating function
 axios.get('https://dino-1-server.herokuapp.com/').then(function(result)   {
-    let apiJobData = result.data;
-    apiJobData.map((job) =>   {
-        let jobList = document.querySelector('#job-listings');
-        let newJob = document.createElement('li');
-        newJob.innerText = job.title;
-        let pay = document.createElement('p');
-        pay.innerHTML = '<small>' + job.pay + '</small>';
-        newJob.appendChild(pay);
-        let desc = document.createElement('p');
-        desc.innerText = job.description;
-        newJob.appendChild(desc);
-        let interestedDinosaurs = document.createElement('p');
-        interestedDinosaurs.innerHTML = '<small></small>';
-        newJob.appendChild(interestedDinosaurs);
-        jobList.appendChild(newJob);
-    });
+    let jobDataFromApi = result.data;
+    jobDataFromApi.map(addToJobList);
 });
 
+//Event listener on Submit button
 let submitButton = document.querySelector('#submit');
-submitButton.addEventListener('click', function(e)   {
-    let title = document.querySelector('#titleInput').value
-    let pay = document.querySelector('#pay').value
-    let description = document.querySelector('#description').value
-    addToJobListings(title, pay, description);
-    e.preventDefault();
-});
+submitButton.addEventListener('click', jobFromInputInfo);
 
-function addToJobListings(title, pay, description) {
+//Create new job card using object containing job info as the parameter
+function addToJobList(job)   {
     let jobList = document.querySelector('#job-listings');
-    let job = document.createElement('li');
-    job.innerText = title;
-    let compensation = document.createElement('p');
-    compensation.innerHTML = '<small>' + pay + '</small>';
-    job.appendChild(compensation);
+    let newJob = document.createElement('li');
+    newJob.innerText = job.title;
+    let pay = document.createElement('p');
+    pay.innerHTML = '<small>' + job.pay + '</small>';
+    newJob.appendChild(pay);
     let desc = document.createElement('p');
-    desc.innerText = description;
-    job.appendChild(desc);
-    jobList.appendChild(job);
-}
+    desc.innerText = job.description;
+    newJob.appendChild(desc);
+    let interestedDinosaurs = document.createElement('p');
+    interestedDinosaurs.innerHTML = '<small></small>';
+    newJob.appendChild(interestedDinosaurs);
+    jobList.appendChild(newJob);
+};
+
+//Make a new job card using user text inputs on the page
+function jobFromInputInfo(e)   {
+    let addJobObject = {'title': document.querySelector('#titleInput').value, 'pay': document.querySelector('#pay').value, 'description': document.querySelector('#description').value};
+    addToJobList(addJobObject);
+    e.preventDefault();
+};
