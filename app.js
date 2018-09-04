e1dceabbef8b513f2f2cac765ba3cdc9c9da49a9
Nicholas Lee <nicholaslee@Nicholass-MacBook-Air-2.local>
2018-09-04 15:06:56 -0600

    made interested value an empty array

--- app.js
+++ app.js
@@ -2,16 +2,14 @@
 //API request and passing result to job card creating function
 axios.get('https://dino-1-server.herokuapp.com/').then(function(result)   {
     let jobDataFromApi = result.data;
-    jobDataFromApi.map(addToJobList);
+    jobDataFromApi.map(createListing);
 });
 
 //Event listener on Submit button
-let submitButton = document.querySelector('#submit');
-submitButton.addEventListener('click', jobFromInputInfo);
+document.querySelector('.job-form').addEventListener('submit', jobFromInputInfo);
 
 //Create new job card using object containing job info as the parameter
-function addToJobList(job)   {
-    let jobList = document.querySelector('#job-listings');
+function createListing(job)   {
     let newJob = document.createElement('li');
     newJob.innerText = job.title;
     let pay = document.createElement('p');
@@ -19,14 +17,15 @@ function addToJobList(job)   {
     let desc = document.createElement('p');
     desc.innerText = job.description;
     let interestedDinosaurs = document.createElement('p');
-    interestedDinosaurs.innerHTML = '<small></small>';
+    interestedDinosaurs.innerHTML = `<small> ${job.interested.length} dinosaurs are interested in this job.</small>`;
     newJob.append(pay, desc, interestedDinosaurs);
+    let jobList = document.querySelector('#job-listings');
     jobList.appendChild(newJob);
 };
 
 //Make a new object using user inputs on the page. pass the object to the job card creating function
 function jobFromInputInfo(e)   {
-    let newJobObject = {'title': document.querySelector('#titleInput').value, 'pay': document.querySelector('#pay').value, 'description': document.querySelector('#description').value};
-    addToJobList(newJobObject);
+    let newJobObject = {'title': document.querySelector('#titleInput').value, 'pay': document.querySelector('#pay').value, 'description': document.querySelector('#description').value, 'interested': []};
+    createListing(newJobObject);
     e.preventDefault();
 };
